ca201fae1c5fbc48c70fd3e3d0144a36aa4a3d9a
da <adad@gmial.com>
2023-07-27 11:48:01 +0530

    name issue fixed

--- components/Innings/Inning.js
+++ components/Innings/Inning.js
@@ -3,16 +3,34 @@ import React, {useState} from 'react';
 import InningList from './InningList';
 
 const Inning = ({data,playersName}) => {
-  const [list, setList] = useState('');
+  const [list, setList] = useState(null);
   // console.log('inning',playersName)
   // console.log(data.Innings[0].AllottedOvers)
   const handleBtn = e => {
     if (e === 'first') {
-      setList('0');
+      setList(0);
+      players(0)
     } else {
-      setList('1');
+      setList(1);
+      players(1)
     }
   };
+  // 
+  const players=(num)=>{
+    
+    for(let i =0; i<data.Innings[num].Batsmen.length;i++){
+      // console.log('inning',inningData.Batsmen[i].Batsman)
+
+      for(let j=0; j<Object.keys(playersName[num]).length;j++){
+        if(data.Innings[num].Batsmen[i].Batsman === Object.keys(playersName[num])[j] ){
+          data.Innings[num].Batsmen[i].Batsman = Object.values(playersName[num])[j]
+          break
+        }
+        // console.log('playrer',Object.keys(playersName)[j])
+      }
+    }
+  }
+  // 
   return (
     <View style={styles.container}>
       <View style={styles.btnCon}>
@@ -26,7 +44,11 @@ const Inning = ({data,playersName}) => {
         </TouchableOpacity>
       </View>
       <View>
-        {list !== '' && <InningList inningData={data.Innings[list]} playersName={playersName[list]} />}
+        {
+        (list !== null) && 
+        <InningList 
+          inningData={data.Innings[list]} 
+        />}
       </View>
     </View>
   );
--- components/Innings/InningList.js
+++ components/Innings/InningList.js
@@ -1,26 +1,12 @@
 import {FlatList, StyleSheet, Text, View} from 'react-native';
 import React, { useEffect } from 'react';
 
-const InningList = ({inningData,playersName}) => {
-  useEffect(()=>{
-    players()
-  })
+const InningList = ({inningData}) => {
+  
   // console.log('data:::', inningData.Number);
   // console.log(playersName);
   // console.log(inningData.Batsmen);
-  const players=()=>{
-    for(let i =0; i<inningData.Batsmen.length;i++){
-      // console.log('inning',inningData.Batsmen[i].Batsman)
-
-      for(let j=0; j<Object.keys(playersName).length;j++){
-        if(inningData.Batsmen[i].Batsman === Object.keys(playersName)[j] ){
-          inningData.Batsmen[i].Batsman = Object.values(playersName)[j]
-          // break
-        }
-        // console.log('playrer',Object.keys(playersName)[j])
-      }
-    }
-  }
+ 
   return (
     <View style={{marginVertical: 20}}>
       <View style={styles.overview}>
@@ -38,7 +24,7 @@ const InningList = ({inningData,playersName}) => {
           }}>
           <Text>Name</Text>
           <Text>Runs</Text>
-          <Text>Balls</Text>
+          {/* <Text>Balls</Text> */}
           <Text>Fours</Text>
           <Text>Sixes</Text>
         </View>
@@ -56,7 +42,7 @@ const InningList = ({inningData,playersName}) => {
             }}>
             <Text>{item.Batsman}</Text>
             <Text>{item.Runs !== '' ? item.Runs : 'NA'}</Text>
-            <Text>{item.Balls !== '' ? item.Balls : 'NA'}</Text>
+            {/* <Text>{item.Balls !== '' ? item.Balls : 'NA'}</Text> */}
             <Text>{item.Fours !== '' ? item.Fours : 'NA'}</Text>
             <Text>{item.Sixes !== '' ? item.Sixes : 'NA'}</Text>
           </View>
